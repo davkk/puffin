59f045501c908e09a64ab6d6bffd799e5f6f1ef2
davkk <40724893+davkk@users.noreply.github.com>
2026-06-14 18:24:35 +0200

    ref: extract idle loop

--- main.go
+++ main.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"context"
-	"log"
 	"os"
 	"os/signal"
 	"puffin/pkg/assert"
 	"puffin/pkg/mail"
-	"time"
 
-	"github.com/emersion/go-imap/v2/imapclient"
 	_ "modernc.org/sqlite"
 )
 
@@ -17,68 +14,10 @@ func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
 
-	wakeUpCh := make(chan struct{}, 1)
-	wakeUp := func() {
-		select {
-		case wakeUpCh <- struct{}{}:
-		default:
-		}
-	}
-	dataHandler := &imapclient.UnilateralDataHandler{
-		Mailbox: func(data *imapclient.UnilateralDataMailbox) {
-			if data.NumMessages != nil {
-				wakeUp()
-			}
-		},
-		Expunge: func(seqNum uint32) {
-			wakeUp()
-		},
-		Fetch: func(msg *imapclient.FetchMessageData) {
-			wakeUp()
-		},
-	}
-
-	imapClient, err := mail.ConnectImapClient("localhost:143", "test", "password", dataHandler)
-	assert.NoError(err, "failed to connect to imap server")
-	defer imapClient.Logout()
-
 	db, err := mail.ConnectSqlite("puffin.db")
 	assert.NoError(err, "failed to connect to sqlite db")
 	defer db.Close()
 
-	for {
-		// TODO: add reconnect logic if tcp dies
-		if err := mail.SyncMailbox(db, imapClient, "INBOX"); err != nil {
-			log.Printf("sync error: %v", err)
-		}
-
-		log.Println("enter idle state")
-		idleCmd, err := imapClient.Idle()
-		if err != nil {
-			log.Printf("IDLE error: %v", err)
-			select {
-			case <-ctx.Done():
-				return
-			case <-time.After(5 * time.Second):
-				continue
-			}
-		}
-
-		select {
-		case <-ctx.Done():
-			idleCmd.Close()
-			idleCmd.Wait()
-			return
-		case <-time.After(29 * time.Minute):
-			log.Println("refresh idle")
-			idleCmd.Close()
-		case <-wakeUpCh:
-			log.Println("server event received")
-			idleCmd.Close()
-		}
-
-		if err := idleCmd.Wait(); err != nil {
-			log.Printf("IDLE wait error: %v", err)
-		}
-	}
+	go mail.NewMailboxWatcher("INBOX").Watch(ctx, db)
+	<-ctx.Done()
 }
---
+++ pkg/mail/watcher.go
@@ -0,0 +1,93 @@
+package mail
+
+import (
+	"context"
+	"database/sql"
+	"log"
+	"puffin/pkg/assert"
+	"time"
+
+	"github.com/emersion/go-imap/v2/imapclient"
+)
+
+// TODO: add watcher per account
+type MailboxWatcher struct {
+	mailboxName string
+	imapClient  *imapclient.Client
+	wakeUpCh    chan struct{}
+}
+
+func NewMailboxWatcher(mailbox string) *MailboxWatcher {
+	return &MailboxWatcher{
+		mailboxName: mailbox,
+		wakeUpCh:    make(chan struct{}),
+	}
+}
+
+func (mw *MailboxWatcher) WakeUp() {
+	select {
+	case mw.wakeUpCh <- struct{}{}:
+	default:
+	}
+}
+
+func (mw *MailboxWatcher) getDataHandler() *imapclient.UnilateralDataHandler {
+	return &imapclient.UnilateralDataHandler{
+		Mailbox: func(data *imapclient.UnilateralDataMailbox) {
+			if data.NumMessages != nil {
+				mw.WakeUp()
+			}
+		},
+		Expunge: func(seqNum uint32) {
+			mw.WakeUp()
+		},
+		Fetch: func(msg *imapclient.FetchMessageData) {
+			mw.WakeUp()
+		},
+	}
+
+}
+
+func (mw *MailboxWatcher) Watch(ctx context.Context, db *sql.DB) {
+	var err error
+	// TODO: extract connection logic
+	mw.imapClient, err = ConnectImapClient("localhost:143", "test", "password", mw.getDataHandler())
+	assert.NoError(err, "failed to connect to imap server")
+	defer mw.imapClient.Logout()
+
+	for {
+		// TODO: add reconnect logic if tcp dies
+		if err := SyncMailbox(db, mw.imapClient, mw.mailboxName); err != nil {
+			log.Printf("sync error: %v", err)
+		}
+
+		log.Println("enter idle state")
+		idleCmd, err := mw.imapClient.Idle()
+		if err != nil {
+			log.Printf("IDLE error: %v", err)
+			select {
+			case <-ctx.Done():
+				return
+			case <-time.After(5 * time.Second):
+				continue
+			}
+		}
+
+		select {
+		case <-ctx.Done():
+			idleCmd.Close()
+			idleCmd.Wait()
+			return
+		case <-time.After(29 * time.Minute):
+			log.Println("refresh idle")
+			idleCmd.Close()
+		case <-mw.wakeUpCh:
+			log.Println("server event received")
+			idleCmd.Close()
+		}
+
+		if err := idleCmd.Wait(); err != nil {
+			log.Printf("IDLE wait error: %v", err)
+		}
+	}
+}
